3f479c6adc50c0b89de14c3feffabb4c9554c3c4
INFJakZda <zdanowski78@gmail.com>
2018-03-24 21:09:38 +0100

    Add email & post code validtion with RegEx (3.33)

--- JavaScript/form_check.js
+++ JavaScript/form_check.js
@@ -1,5 +1,6 @@
 var errorField = "";
 
+//**************************//
 function isEmpty(str) {
     if(!str.length || str == undefined) {
         return true;
@@ -8,6 +9,7 @@ function isEmpty(str) {
     }
 }
 
+//**************************//
 function isWhiteSpace(str) {
     var ws = "\t\n\r ";
     for (var i = 0; i < str.length; i++) {
@@ -19,8 +21,9 @@ function isWhiteSpace(str) {
     return true;
  }
 
+//**************************//
 function validate(form) {
-    var checkList = ["imie", "nazwisko", "kod", "ulica", "miasto"];
+    var checkList = ["imie", "nazwisko", "ulica", "miasto"];
     var flag = true;
     for(i = 0; i < checkList.length; i++) {
         var tmp = checkList[i];
@@ -31,13 +34,21 @@ function validate(form) {
         }
     }
 
-    flag = checkEmail(form.elements["f_email"].value)
+    flag = checkEmailRegEx(form.elements["f_email"].value)
     if(!flag) {
+        return false;
+    }
+
+    var postCode = form.elements["f_kod"].value;
+    if(checkZIPCodeRegEx(postCode)) {
+        alert("Zly kod pocztowy");
+        form.elements["f_kod"].focus();
         return false;
     }
     return true;  
 }
 
+//**************************//
 function checkString(str, msg) {
     if(isEmpty(str) || isWhiteSpace(str)) {
         alert(msg);
@@ -46,7 +57,22 @@ function checkString(str, msg) {
         return true;
     }
 }
+//**************************//
 
+function checkZIPCodeRegEx(str) {
+    var code = /[0-9]{2}-[0-9]{3}/;
+    if (code.test(str) && str.length == 6) {
+        document.getElementById("kod").innerHTML = "OK";
+        document.getElementById("kod").className = "green";
+        return false;
+    } else {
+        document.getElementById("kod").innerHTML = "źle";
+        document.getElementById("kod").className = "red";
+        return true;
+    }
+}
+
+//**************************//
 function checkEmail(str) {
     if (isWhiteSpace(str)) {
         alert("Podaj właściwy e-mail");
@@ -75,6 +101,19 @@ function checkEmail(str) {
   }
 }
 
+//**************************//
+function checkEmailRegEx(str) {
+    var email = /[a-zA-Z_0-9\.]+@[a-zA-Z_0-9\.]+\.[a-zA-Z][a-zA-Z]+/;
+    if (email.test(str))
+        return true;
+    else {
+        alert("Podaj właściwy e-mail");
+        //document.elements["f_email"].focus();
+        return false;
+    }
+}
+
+//**************************//
 function checkStringAndFocus(obj, msg) {
     var str = obj.value;
     var errorFieldName = "e_" + obj.name.substr(2, obj.name.length);
@@ -89,19 +128,23 @@ function checkStringAndFocus(obj, msg) {
     }
  }
 
+//**************************//
 function startTimer(fName) {
     errorField = fName;
     window.setTimeout("clearError(errorField)", 1000);
 }
 
+//**************************//
 function clearError(objName) {
     document.getElementById(objName).innerHTML = "";
 }
  
+//**************************//
 function showElement(e) {
     document.getElementById(e).style.visibility = 'visible';
 }
 
+//**************************//
 function hideElement(e) {
     document.getElementById(e).style.visibility = 'hidden';
 }

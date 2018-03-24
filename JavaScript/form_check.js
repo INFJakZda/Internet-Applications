ff06deb355e5efb9091abcf28a0319b88620234b
INFJakZda <zdanowski78@gmail.com>
2018-03-24 17:40:13 +0100

    Add timer to validation (3.21)

--- JavaScript/form_check.js
+++ JavaScript/form_check.js
@@ -1,88 +1,100 @@
+var errorField = "";
+
 function isEmpty(str) {
-  if(!str.length || str == undefined) {
-    return true;
-  } else {
-    return false
-  }
+    if(!str.length || str == undefined) {
+        return true;
+    } else {
+        return false
+    }
 }
 
 function isWhiteSpace(str) {
-  var ws = "\t\n\r ";
-  for (var i = 0; i < str.length; i++) {
-    var c = str.charAt(i);
-    if (ws.indexOf(c) == -1) {
-      return false;
+    var ws = "\t\n\r ";
+    for (var i = 0; i < str.length; i++) {
+        var c = str.charAt(i);
+        if (ws.indexOf(c) == -1) {
+            return false;
+        }
     }
-  }
-  return true;
+    return true;
  }
 
 function validate(form) {
-  var checkList = ["imie", "nazwisko", "kod", "ulica", "miasto"];
-  var flag = true;
-  for(i = 0; i < checkList.length; i++) {
-    var tmp = checkList[i];
-    //flag = checkString(form.elements["f_" + tmp].value, "Podaj " + tmp);
-    flag = checkStringAndFocus(form.elements["f_" + tmp], "Podaj " + tmp);
-    if(!flag) {
-      return false;
+    var checkList = ["imie", "nazwisko", "kod", "ulica", "miasto"];
+    var flag = true;
+    for(i = 0; i < checkList.length; i++) {
+        var tmp = checkList[i];
+        //flag = checkString(form.elements["f_" + tmp].value, "Podaj " + tmp);
+        flag = checkStringAndFocus(form.elements["f_" + tmp], "Podaj " + tmp);
+        if(!flag) {
+            return false;
+        }
     }
-  }
 
-  flag = checkEmail(form.elements["f_email"].value)
-  if(!flag) {
-    return false;
-  }
-  return true;  
+    flag = checkEmail(form.elements["f_email"].value)
+    if(!flag) {
+        return false;
+    }
+    return true;  
 }
 
 function checkString(str, msg) {
-  if(isEmpty(str) || isWhiteSpace(str)) {
-    alert(msg);
-    return false;
-  } else {
-    return true;
-  }
+    if(isEmpty(str) || isWhiteSpace(str)) {
+        alert(msg);
+        return false;
+    } else {
+        return true;
+    }
 }
 
 function checkEmail(str) {
-  if (isWhiteSpace(str)) {
-    alert("Podaj właściwy e-mail");
-    return false;
-  } 
-  else {
-    var at = str.indexOf("@");
-    if (at < 1) {
-      alert("Nieprawidłowy e-mail");
-      return false;
+    if (isWhiteSpace(str)) {
+        alert("Podaj właściwy e-mail");
+        return false;
     } 
     else {
-      var l = -1;
-      for (var i = 0; i < str.length; i++) {
-        var c = str.charAt(i);
-        if (c == ".") {
-          l = i;
+        var at = str.indexOf("@");
+        if (at < 1) {
+            alert("Nieprawidłowy e-mail");
+            return false;
+        } 
+        else {
+            var l = -1;
+            for (var i = 0; i < str.length; i++) {
+                var c = str.charAt(i);
+                if (c == ".") {
+                l = i;
+            }
+            }
+            if ((l < (at + 2)) || (l == str.length - 1)) {
+                alert("Nieprawidłowy e-mail");
+                return false;
+            }
         }
-      }
-      if ((l < (at + 2)) || (l == str.length - 1)) {
-        alert("Nieprawidłowy e-mail");
-        return false;
-      }
-    }
-    return true;
+        return true;
   }
 }
 
 function checkStringAndFocus(obj, msg) {
-  var str = obj.value;
-  var errorFieldName = "e_" + obj.name.substr(2, obj.name.length);
-  if (isWhiteSpace(str) || isEmpty(str)) {
-    document.getElementById(errorFieldName).innerHTML = msg;
-    obj.focus();
-    return false;
-  }
-  else {
-    return true;
-  }
+    var str = obj.value;
+    var errorFieldName = "e_" + obj.name.substr(2, obj.name.length);
+    if (isWhiteSpace(str) || isEmpty(str)) {
+        document.getElementById(errorFieldName).innerHTML = msg;
+        obj.focus();
+        startTimer(errorFieldName);
+        return false;
+    }
+    else {
+        return true;
+    }
  }
+
+function startTimer(fName) {
+    errorField = fName;
+    window.setTimeout("clearError(errorField)", 1000);
+}
+
+function clearError(objName) {
+    document.getElementById(objName).innerHTML = "";
+}
  

70b05732e59bc511a7072d8e863a62b72585d712
INFJakZda <zdanowski78@gmail.com>
2018-04-29 22:50:07 +0200

    Add simple content return to SongsController - nr to the roots

---
+++ ASP.NET-MVC-Song/MvcSong/MvcSong/Controllers/SongsController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcSong.Controllers
+{
+    public class SongsController : Controller
+    {
+        // GET: Songs
+        public ActionResult Index()
+        {
+            return Content("Hello World!");
+        }
+
+        public ActionResult Square(int id)
+        {
+            return Content((id * id).ToString());
+        }
+
+        public ActionResult Square2()
+        {
+            return Content((23 * 23).ToString());
+        }
+    }
+}

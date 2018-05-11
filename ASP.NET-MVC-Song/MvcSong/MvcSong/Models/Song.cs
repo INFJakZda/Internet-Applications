af474176e878288c548ac44ec84357285464d363
INFJakZda <zdanowski78@gmail.com>
2018-05-11 16:11:07 +0200

    Add models & connect to db

---
+++ ASP.NET-MVC-Song/MvcSong/MvcSong/Models/Song.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcSong.Models
+{
+    public class Song
+    {
+        int Id;
+        string Name;
+        string Artist;
+        string Genre;
+
+        public Song(int id, string name, string artist, string genre)
+        {
+            this.Id = id;
+            this.Name = name;
+            this.Artist = artist;
+            this.Genre = genre;
+        }
+
+        public String getName() { return Name; }
+    }
+}
